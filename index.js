4406590509f140f1c2665eca63c5bb32d58d83e9
Taher027 <abutaherh27@gmail.com>
2022-11-28 12:29:22 +0600

    setup server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+
+app.use(cors());
+app.use(express())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iitoxlh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+app.get('/', async (req, res) => {
+    res.send('used product server running');
+})
+
+app.listen(port, ()=>console.log(`server running on ${port}`))
