716dca77d76395b0252037991f2bc05460add85a
Taher027 <abutaherh27@gmail.com>
2022-11-29 12:15:08 +0600

    store user to database

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { restart } = require('nodemon');
+const app = express();
 const port = process.env.PORT || 5000;
 
-const app = express();
-
-// middleware
-
+// middle wares
 app.use(cors());
-app.use(express())
-
-
-
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iitoxlh.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    
+
+    const userCollection = client.db('assignment').collection('users');
 
     try {
-        
+
+        // user collection to db
+
+
+        app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            const result = await userCollection.insertOne(userInfo);
+            res.send(result);
+        })
+
     }
-    catch {
-        console.log("error from server");
+    finally {
+
     }
 }
+run().catch(err => console.log(err))
 
+app.get('/', (req, res) => {
+    console.log('test')
+    res.send('api found')
 
-
-app.get('/', async (req, res) => {
-    res.send('used product server running');
 })
-
-app.listen(port, ()=>console.log(`server running on ${port}`))
+app.listen(port, () => {
+    console.log('server running')
+})
