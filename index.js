f6daf4e2cebf3389475d16ca737cf13f3cb8eab0
Taher027 <abutaherh27@gmail.com>
2022-12-02 14:53:15 +0600

    add delete product from database

--- index.js
+++ index.js
@@ -18,27 +18,57 @@ async function run() {
 
     const userCollection = client.db('assignment').collection('users');
     const productCollection = client.db('assignment').collection('products');
+    const advertiseCollection = client.db('assignment').collection('advertise');
 
     try {
 
         // user collection to db
 
-
-        app.post('/users', async (req, res) => {
+        //insert user info
+        app.post('/user', async (req, res) => {
             const userInfo = req.body;
             const result = await userCollection.insertOne(userInfo);
             res.send(result);
         });
-        app.delete("/deleteUser/:email", async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const result = await userCollection.deleteOne(query);
+
+
+        //insert advertise info
+        app.post('/advertise_product', async (req, res) => {
+            const advertiseInfo = req.body;
+            const result = await advertiseCollection.insertOne(advertiseInfo);
             res.send(result);
-        })
+        });
+        //add product
         app.post('/addProduct', async (req, res) => {
             const productInfo = req.body;
             console.log(productInfo);
             const result = await productCollection.insertOne(productInfo);
+            res.send(result);
+        });
+
+        //find a specific seller product
+        app.get('/dashboard/my_products/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {SellerEmail: email}
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        //deleted
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            };
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        });
+
+        // find all advertisement product 
+        app.get('advertiseItems', async (req, res) => {
+            const query = {}
+            const cursor = advertiseCollection.find(query);
+            const result = await cursor.toArray;
             res.send(result);
         })
 
