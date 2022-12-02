1d749aa7954229d886b06ea5ee408abd65f2549e
Taher027 <abutaherh27@gmail.com>
2022-12-02 21:10:24 +0600

    load booking item

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ async function run() {
     const userCollection = client.db('assignment').collection('users');
     const productCollection = client.db('assignment').collection('products');
     const advertiseCollection = client.db('assignment').collection('advertise');
+    const reportCollection = client.db('assignment').collection('report');
+    const bookingCollection = client.db('assignment').collection('booking');
 
     try {
 
@@ -31,6 +33,34 @@ async function run() {
             res.send(result);
         });
 
+        //insert user info
+        app.post('/booking', async (req, res) => {
+            const bookingInfo = req.body;
+            const result = await bookingCollection.insertOne(bookingInfo);
+            res.send(result);
+        });
+        app.get('/myOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { BuyerEmail: email };
+            const cursor = bookingCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        //get user info
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+        //all users
+        app.get('/dashboard/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
         //insert advertise info
         app.post('/advertise_product', async (req, res) => {
@@ -46,6 +76,14 @@ async function run() {
             res.send(result);
         });
 
+
+        //report
+        app.post('/report', async (req, res) => {
+            const productInfo = req.body;
+            const result = await reportCollection.insertOne(productInfo);
+            res.send(result);
+        });
+
         //find a specific seller product
         app.get('/dashboard/my_products/:email', async (req, res) => {
             const email = req.params.email;
@@ -54,6 +92,17 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         });
+
+        //find a specific category product
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { Category: id }
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
         //deleted
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
@@ -64,10 +113,46 @@ async function run() {
             res.send(result)
         });
 
+
+        //deleted reported item
+        app.delete('/deleteProduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const Reportquery = {
+                _id: id
+            };
+            const Productquery = {
+                _id: ObjectId(id)
+            };
+            const Advertisequery = {
+                _id: id
+            };
+
+            const result = await productCollection.deleteOne(Productquery);
+            const result1 = await reportCollection.deleteOne(Reportquery);
+            const result2 = await advertiseCollection.deleteOne(Advertisequery);
+            res.send(result)
+        });
+        // find category product 
+        app.get("/category", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // find all advertisement product 
         app.get("/advertiseItems", async (req, res) => {
             const query = {};
             const cursor = advertiseCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        // find all report
+        app.get("/report", async (req, res) => {
+            const query = {};
+            const cursor = reportCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
